499f7c574c1a6c816461b6dfdb9aec344d291f14
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2024-04-04 09:55:16 -0300

    Inicialización del proyecto 🚀

---
+++ Dashboard Buen Sabor/src/main/java/com/hardcodeo/buenSabor/BuenSaborApplication.java
@@ -0,0 +1,16 @@
+package com.hardcodeo.buenSabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuenSaborApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BuenSaborApplication.class, args);
+
+
+	}
+
+}
