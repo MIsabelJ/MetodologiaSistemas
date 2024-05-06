0b6898c7c109ed96aa78a4cb33ee024fadceb4a9
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-05-06 11:41:40 -0300

    Base Dto

---
+++ src/main/java/com/example/buensaborback/domain/dtos/BaseDto.java
@@ -0,0 +1,18 @@
+package com.example.buensaborback.domain.dtos;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseDto {
+
+    private long id;
+    private boolean eliminado;
+}
